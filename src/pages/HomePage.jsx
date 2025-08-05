618b6bb4b422f48d2d7c7c996feeed886bdcdb12
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2025-08-05 19:43:30 +0200

    Ajout du composant Carousel avec animations GSAP, intégration de nouvelles images et mise à jour des styles SCSS. Modification de la configuration Vite pour inclure le plugin vite-plugin-svgr et mise à jour des dépendances dans package.json et package-lock.json.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,31 +2,14 @@ import React, { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../App.css";
+import Carousel from "../components/Carrousel/Carousel";
 
 const HomePage = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="home">
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
-        <p>
-          Modifiez <code>src/pages/HomePage.jsx</code> et sauvegardez pour
-          tester le HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Cliquez sur les logos Vite et React pour en savoir plus
-      </p>
+      <Carousel />
     </div>
   );
 };
