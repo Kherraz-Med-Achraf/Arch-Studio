67e73319a1aa946a6557fb1a11ce826bca4e7e53
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2025-08-25 15:02:43 +0200

    Ajout du composant TeamLeaders avec styles SCSS associés, intégration des leaders d'équipe et de leurs avatars dans la page À propos pour améliorer la présentation de l'équipe.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutHero from "../components/AboutHero/AboutHero";
 import Heritage from "../components/Heritage/Heritage";
+import TeamLeaders from "../components/TeamLeaders/TeamLeaders";
 
 const AboutPage = () => {
   return (
     <section className="projects-page">
       <AboutHero />
       <Heritage />
+      <TeamLeaders />
     </section>
   );
 };
